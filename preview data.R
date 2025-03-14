5c2a779698da0897d9430a4d94e62cce78bfc288
YangWang666 <77311772+YangWang666@users.noreply.github.com>
2025-03-14 13:59:37 -0700

    Preview of dataset
    Preview of SUPPORT Datasets and VA Lung Cancer Dataset

---
+++ preview data.R
@@ -0,0 +1,7 @@
+data1<- read.csv("https://raw.githubusercontent.com/enignans/STAT447C_project/refs/heads/main/support2.csv")
+head(data1)
+str(data1)
+
+data2<-read.csv("https://raw.githubusercontent.com/enignans/STAT447C_project/refs/heads/main/valung.csv")
+head(data2)
+str(data2)
